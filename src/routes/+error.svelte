c804c75073f4ca2bf918060607d2ef1f3e560541
Rob Manders <github.powdered589@passmail.net>
2025-06-09 21:57:12 +0100

    Set as static app and create 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="container">
+	<h2>404</h2>
+
+	<p>Page not found</p>
+</div>
+
+<style lang="scss">
+	h2 {
+		font-size: 1.25rem;
+		font-family: "Calvert", sans-serif;
+		text-align: center;
+	}
+
+	.container {
+		text-align: center;
+		padding: 0 0.5rem;
+	}
+</style>
